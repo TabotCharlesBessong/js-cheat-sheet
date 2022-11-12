9e2f53c646b07831dbd7590a6a9526e9e4c47df5
Charles Bessong Tabot <82298650+TabotCharlesBessong@users.noreply.github.com>
2022-11-12 06:28:01 +0100

    Create Element.js

---
+++ basic/Element.js
@@ -0,0 +1,9 @@
+function MyComponent() {
+  return (
+    <>
+//     React element also uses html element 
+      <h1 style={{ fontSize: 24, margin: '0 auto', textAlign: 'center' }}>My header</h1>
+      </p>My paragraph</p>
+    </>
+  );
+}
